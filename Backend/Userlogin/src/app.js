17725b066fe5bbc8cd8885741988d6f27b7fb1c8
Ritam Haldar <rickhaldar561@gmail.com>
2026-02-18 23:39:22 +0530

    Login

--- Backend/Userlogin/src/app.js
+++ Backend/Userlogin/src/app.js
@@ -5,12 +5,4 @@ const authroute= require("./routes/auth.routes");
 app.use(cookieparser());
 app.use(express.json());
 app.use("/api/auth",authroute);
-// JSON parse error handler
-app.use((err, req, res, next) => {
-	if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-		return res.status(400).json({ error: 'Invalid JSON payload' });
-	}
-	next(err);
-});
-
 module.exports = app;
